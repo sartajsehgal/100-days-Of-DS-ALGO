73ca6c0e6ad00e63f724e26133047cc689476c29
sartajsehgal <sartajsehgal16@gmail.com>
2021-05-19 22:02:14 +0530

    Day 2

---
+++ Recursion/merge_sort.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void merge_sort(int arr[],int l,int r)
+{
+
+}
+
+int main()
+{
+	int arr[]={1,6,2,7,8,3};
+	int n=sizeof(arr)/sizeof(int);
+	//bubble_sort(arr,n,0);
+	//bubble_sort1(arr,n); 
+	for(int i=0;i<n;i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+	return 0;
+}
